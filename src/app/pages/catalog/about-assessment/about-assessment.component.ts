4e1a380fd8b354dd316eb38a843cb084a2699060
thrivikraman.r <thrivikraman.r@lntecc.com>
2022-01-20 12:33:17 +0530

    Task 17733: Portal assessmentMapping JSON structure change for new Course Section Task 17734: Portal Tabs Binding change

--- src/app/pages/catalog/about-assessment/about-assessment.component.ts
+++ src/app/pages/catalog/about-assessment/about-assessment.component.ts
@@ -123,7 +123,7 @@ export class AboutAssessmentComponent implements OnInit {
         } else {
             this.sticky = false;
         }
-       //this.checkOffsetTop();
+       this.checkOffsetTop();
         setTimeout(()=>{
             if(window.pageYOffset < this.menuPosition){
               this.activeSection = null;
@@ -164,7 +164,6 @@ export class AboutAssessmentComponent implements OnInit {
            this.fifthOffset =  this.fifthElement.nativeElement.offsetTop - 200;
            this.sixthOffset =  this.sixthElement.nativeElement.offsetTop - 200;
            this.seventhOffset =  this.seventhElement.nativeElement.offsetTop -200;
-           console.log(this.seventhOffset);
         },1000)
     }
   ngOnInit(): void {
