afdd893eba1c898ddd370b40fb80e4c01e6e8e12
Karthikeyan <karthikeyanb@lntecc.com>
2022-01-24 11:51:44 +0530

    Task 17829: Owl Carousel Navigation and slide UI implemented for Related Programs and Related Assessments

--- src/app/pages/catalog/about-assessment/about-assessment.component.ts
+++ src/app/pages/catalog/about-assessment/about-assessment.component.ts
@@ -65,6 +65,31 @@ export class AboutAssessmentComponent implements OnInit {
       }
     }
   }
+  //
+  relatedList: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    margin: 30,
+    navText: ["<i class='icon-Back'></i>", "<i class='icon-right-next'></i>"],
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      992: {
+        items: 4,
+        slideBy: 4
+      }
+    }
+  }
+
   howItWorks:any
   isReadMore = true
   areaId;
