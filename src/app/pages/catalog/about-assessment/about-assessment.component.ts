62a05258d42b464d92e7ec85aee4348406d554f0
Karthikeyan <karthikeyanb@lntecc.com>
2022-01-24 14:17:57 +0530

    Task 17834: UI updated for Our partners section, Export Support System, More help section
    Task 17834: UI updated for Our partners section, Export Support System, More help section

--- src/app/pages/catalog/about-assessment/about-assessment.component.ts
+++ src/app/pages/catalog/about-assessment/about-assessment.component.ts
@@ -26,11 +26,11 @@ export class AboutAssessmentComponent implements OnInit {
     mouseDrag: false,
     touchDrag: false,
     pullDrag: false,
-    dots: true,
+    dots: false,
     navSpeed: 700,
     margin: 30,
     navText: ["<i class='icon-LeftArrow'></i>", "<i class='icon-RightArrow'></i>"],
-    nav: false,
+    nav: true,
     responsive: {
       0: {
         items: 1
@@ -48,11 +48,11 @@ export class AboutAssessmentComponent implements OnInit {
     mouseDrag: false,
     touchDrag: false,
     pullDrag: false,
-    dots: true,
+    dots: false,
     navSpeed: 700,
     margin: 30,
     navText: ["<i class='icon-LeftArrow'></i>", "<i class='icon-RightArrow'></i>"],
-    nav: false,
+    nav: true,
     responsive: {
       0: {
         items: 1
