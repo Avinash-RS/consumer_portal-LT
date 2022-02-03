1c0606cd7f5c9285e4673f602cb0346b951b27df
thrivikraman.r <thrivikraman.r@lntecc.com>
2022-02-03 09:50:17 +0530

    Task 18194: Portal About assessment Related items common component for about assessment and course page

--- src/app/shared/relateditems/relateditems.component.ts
+++ src/app/shared/relateditems/relateditems.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-relateditems',
@@ -8,6 +9,33 @@ import { Component, Input, OnInit } from '@angular/core';
 export class RelateditemsComponent implements OnInit {
 
 @Input('relateditem') item:any;
+relatedList: OwlOptions = {
+  loop: true,
+  mouseDrag: false,
+  touchDrag: false,
+  pullDrag: false,
+  dots: true,
+  navSpeed: 700,
+  margin: 30,
+  navText: ["<i class='icon-Back'></i>", "<i class='icon-right-next'></i>"],
+  nav: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    700: {
+      items: 3,
+      slideBy: 3
+    },
+    1024: {
+      items: 4,
+      slideBy: 4
+    }
+  }
+}
   constructor() { }
 
   ngOnInit(): void {
