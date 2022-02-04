fb520342c46ed121861f3e446ca136b79ef77471
thrivikraman.r <thrivikraman.r@lntecc.com>
2022-02-04 15:34:01 +0530

    Task 18237: Portal landing Page API binding

--- src/app/pages/dashboard/home/home.component.ts
+++ src/app/pages/dashboard/home/home.component.ts
@@ -22,102 +22,9 @@ export class HomeComponent implements OnInit {
   commonData: any;
   showdialog: boolean = false;
   userDetails: any;
-  OurPartners =  {
-    "sectionId" : 11,
-    "dispalystatus" : true,
-    "title" : "Hiring Partners",
-    "imageurl" : "https://lmsassetspremium.lntedutech.com/portalicons/hiringPartners_bg.png",
-    "description" : "L&T EduTech collaborates with 100+ recruiting partners, identify and understand the key talent expectations trending across the globe, design curriculum on right technologies, train learners with industry oriented practice eco-system to place them successfully.",
-    "innerArray" : [ 
-        {
-            "title" : "L&T Construction",
-            "imageurl" : "https://lmsassetspremium.lntedutech.com/portalicons/lntcc.png"
-        }, 
-        {
-            "title" : "L&T NXT",
-            "imageurl" : "https://lmsassetspremium.lntedutech.com/portalicons/nxt.png"
-        }, 
-        {
-            "title" : "L&T Technology",
-            "imageurl" : "https://lmsassetspremium.lntedutech.com/portalicons/techservice.png"
-        }, 
-        {
-            "title" : "LTI",
-            "imageurl" : "https://lmsassetspremium.lntedutech.com/portalicons/lti.png"
-        }, 
-        {
-            "title" : "Mind Tree Construction",
-            "imageurl" : "https://lmsassetspremium.lntedutech.com/portalicons/mindtree.png"
-        }
-    ],
-    "subHeading1" : {
-        "dispalystatus" : true,
-        "title" : "Programs",
-        "imageurl" : "",
-        "description" : "",
-        "innerArray" : [ 
-            "Full Stack .NET Application Development Cyber Security", 
-            "Python and Data Management", 
-            "MEAN Application Development"
-        ]
-    },
-    "subHeading2" : {
-        "dispalystatus" : true,
-        "title" : "Courses",
-        "imageurl" : "",
-        "description" : "",
-        "innerArray" : [ 
-            "Modern Web Design", 
-            "Angular 7", 
-            "Complete Node JS", 
-            "MongoDB", 
-            "Complete Java", 
-            "MySQL", 
-            "Modern Web Design", 
-            "Spring Boot", 
-            "Complete C#", 
-            "ASP.NET", 
-            "Data Science and Machine Learning with Python", 
-            "Data Visualization with PowerBI", 
-            "Restful Services with ASP.NET", 
-            "Web Services with Java"
-        ]
-    },
-    "subHeading3" : {
-        "dispalystatus" : true,
-        "title" : "Our Partners :",
-        "imageurl" : "",
-        "description" : "students can reach us at – call: 000-000-0000 email: xyz@lntedutech.com",
-        "innerArray" : [ 
-            {
-                "title" : "Pack",
-                "imageurl" : "https://lmsassetspremium.lntedutech.com/portalicons/packt.png"
-            }, 
-            {
-                "title" : "Pack",
-                "imageurl" : "https://lmsassetspremium.lntedutech.com/portalicons/step.png"
-            }, 
-            {
-                "title" : "Pack",
-                "imageurl" : "https://lmsassetspremium.lntedutech.com/portalicons/wecp.png"
-            }, 
-            {
-                "title" : "Pack",
-                "imageurl" : "https://lmsassetspremium.lntedutech.com/portalicons/amphisoft.png"
-            }
-        ]
-    },
-    "subHeading4" : {
-        "dispalystatus" : true,
-        "title" : "Payment Powered by :",
-        "imageurl" : "https://lmsassetspremium.lntedutech.com/portalicons/cc.png",
-        "Subimageurl" : "https://lmsassetspremium.lntedutech.com/portalicons/ccavenue.png",
-        "description" : ""
-    }
-}
-HomeBannerImage = [
-  1,2,3,4
-];
+  HomeBannerImage = [
+    1,2,3,4
+  ];
 activeButton = '';
 testimonialOptions: OwlOptions = {
   loop: true,
@@ -209,8 +116,6 @@ testimonialOptions: OwlOptions = {
     this.commonService.verifySsotoken(token).subscribe((response: any) => {
       if (response && response.token) {
         var decoded = jwt_decode(token);
-        console.log(decoded);
-
         this.cookieService.set('isLoggedIn', 'true')
       }
     })
